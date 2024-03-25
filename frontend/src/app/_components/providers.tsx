9b9b1ba02b1a85ca8e651ecd0edaefc532b5a34f
Rathijit Paul <alienpapon009@gmail.com>
2024-03-25 20:29:00 +0600

    :construction: update analytics tracking control with env

--- frontend/src/app/_components/providers.tsx
+++ frontend/src/app/_components/providers.tsx
@@ -6,7 +6,9 @@ import { Toaster } from "react-hot-toast"
 import posthog from "posthog-js"
 import { PostHogProvider } from 'posthog-js/react'
 
-if (typeof window !== 'undefined') {
+const posthog_enabled = process.env.NEXT_PUBLIC_POSTHOG_KEY != null
+
+if (typeof window !== 'undefined' && posthog_enabled) {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY || '', {
     api_host: process.env.NEXT_PUBLIC_POSTHOG_API_HOST || '',
     ui_host: process.env.NEXT_PUBLIC_POSTHOG_UI_HOST || '',
