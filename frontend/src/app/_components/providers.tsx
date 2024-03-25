0efbfcd244c6754ac7a2818e0611104b64596516
Rathijit Paul <alienpapon009@gmail.com>
2024-03-25 18:57:49 +0600

    :sparkles: posthog analytics added

--- frontend/src/app/_components/providers.tsx
+++ frontend/src/app/_components/providers.tsx
@@ -3,6 +3,24 @@
 import { Analytics } from "@vercel/analytics/react"
 import { AxiomWebVitals } from "next-axiom"
 import { Toaster } from "react-hot-toast"
+import posthog from "posthog-js"
+import { PostHogProvider } from 'posthog-js/react'
+
+if (typeof window !== 'undefined') {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_API_HOST,
+    capture_pageview: false,
+  })
+}
+
+export function PosthogProvider({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return <PostHogProvider client={posthog}>{children}</PostHogProvider>
+}
+
 
 export function Providers() {
   return (
--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,9 +2,10 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Link from "next/link"
 import { cn } from "@/lib/utils"
-import { Providers } from "./_components/providers"
+import { Providers, PosthogProvider } from "./_components/providers"
 import Image from 'next/image';
 import "./globals.css";
+import dynamic from 'next/dynamic'
 
 const BODY_PADDING = "px-4 sm:px-6"
 
@@ -15,6 +16,10 @@ export const metadata: Metadata = {
   description: "Next generation healthcare search engine",
 };
 
+const PostHogPageView = dynamic(() => import('./PostHogPageView'), {
+  ssr: false,
+})
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -22,32 +27,35 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={cn(inter.className, "antialiased bg-gray-100")}>
-        <header
-          className={cn(
-            "top-0 sticky z-20 w-full py-3 bg-gray-100 flex flex-row flex-nowrap justify-between max-w-5xl mx-auto h-14 items-stretch animate-in fade-in slide-in-from-top-4 duration-1000 ease-in-out",
-            BODY_PADDING
-          )}
-        >
-
-          <Link
-            className="text-black text-lg font-medium flex flex-row flex-nowrap items-center justify-center gap-x-1.5 pr-1.5 leading-none rounded-lg"
-            href="/"
+      <PosthogProvider>
+        <body className={cn(inter.className, "antialiased bg-gray-100")}>
+          <header
+            className={cn(
+              "top-0 sticky z-20 w-full py-3 bg-gray-100 flex flex-row flex-nowrap justify-between max-w-5xl mx-auto h-14 items-stretch animate-in fade-in slide-in-from-top-4 duration-1000 ease-in-out",
+              BODY_PADDING
+            )}
           >
-            <Image
-              src="/icon.png"
-              width={24}
-              height={24}
-              alt="Picture of the author"
-            />
-            <span>Curieo</span>
-          </Link>
-        </header>
-        <main className={cn("min-h-screen flex items-stretch flex-col pb-28 max-w-5xl mx-auto", BODY_PADDING)}>
-          {children}
-        </main>
-        <Providers />
-      </body>
+
+            <Link
+              className="text-black text-lg font-medium flex flex-row flex-nowrap items-center justify-center gap-x-1.5 pr-1.5 leading-none rounded-lg"
+              href="/"
+            >
+              <Image
+                src="/icon.png"
+                width={24}
+                height={24}
+                alt="Picture of the author"
+              />
+              <span>Curieo</span>
+            </Link>
+          </header>
+          <main className={cn("min-h-screen flex items-stretch flex-col pb-28 max-w-5xl mx-auto", BODY_PADDING)}>
+            <PostHogPageView />
+            {children}
+          </main>
+          <Providers />
+        </body>
+      </PosthogProvider>
     </html>
   )
 }
