ba7c79362ddf570a069c4e87e4f49cc58f33043b
Ivar Flakstad <69173633+ivarflakstad@users.noreply.github.com>
2024-04-08 19:23:40 +0200

    Add from_settings helper to response reranker

--- backend/app/rag/reranker/response_reranker.py
+++ backend/app/rag/reranker/response_reranker.py
@@ -17,28 +17,31 @@ logger = setup_logger("TextEmbeddingInferenceRerankEngine")
 
 class TextEmbeddingInferenceRerankEngine(BaseNodePostprocessor):
     """
-    The class extends the BaseNodePostprocessor class, aimed at reranking nodes (elements) based on text embedding inference.
-    This class is part of a larger framework, likely for processing and analyzing data within a specific domain,
-    such as document retrieval or search engine optimization. Here's an overview of the class and its components:
+    The class extends the BaseNodePostprocessor class, aimed at reranking nodes
+    (elements) based on text embedding inference. This class is part of a larger
+    framework, likely for processing and analyzing data within a specific domain,
+    such as document retrieval or search engine optimization. Here's an overview of
+    the class and its components:
     """
 
     model: str = Field(
         default="BAAI/bge-reranker-large",
         description="The model to use when calling AI API",
     )
+    api: str
+    top_count: int
     _session: Any = PrivateAttr()
 
-    def __init__(
-        self,
-        settings: RerankSettings,
-        top_n: int = 2,
-        model: str = "BAAI/bge-reranker-large",
-    ):
-        super().__init__(top_n=top_n, model=model)
-        self.api = settings.api
-        self.top_count = settings.top_count
+    def __init__(self, *, api: str, top_count: int, model: str, top_n: int = 2):
+        super().__init__(top_n=top_n, model=model, api=api, top_count=top_count)
         self.model = model
         self._session = requests.Session()
+
+    @classmethod
+    def from_settings(
+        cls, *, settings: RerankSettings
+    ) -> "TextEmbeddingInferenceRerankEngine":
+        return cls(api=settings.api, top_count=settings.top_count, model=settings.model)
 
     @classmethod
     def class_name(cls) -> str:
@@ -50,13 +53,17 @@ class TextEmbeddingInferenceRerankEngine(BaseNodePostprocessor):
         query_bundle: Optional[QueryBundle] = None,
     ) -> List[NodeWithScore]:
         """
-        This method takes a list of nodes (each represented by a NodeWithScore object) and an optional QueryBundle object.
+        This method takes a list of nodes (each represented by a NodeWithScore object)
+        and an optional QueryBundle object.
         It performs the reranking operation by:
         -- Validating the input.
         -- Extracting text from each node, removing HTML tags.
-        -- Sending a request to the specified AI API with the extracted texts and additional query information.
-        -- Processing the API's response to update the nodes' scores based on reranking results.
-        -- Returning the top N reranked nodes, according to the class's top_n attribute and possibly constrained by a global RERANK_TOP_COUNT.
+        -- Sending a request to the specified AI API with the extracted texts and
+           additional query information.
+        -- Processing the APIs response to update the nodes' scores based on reranking
+           results.
+        -- Returning the top N reranked nodes, according to the class's top_n attribute
+           and possibly constrained by top_count
         """
         if query_bundle is None:
             raise ValueError("Missing query bundle in extra info.")
