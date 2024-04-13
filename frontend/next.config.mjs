2e5006783ea79fedc07626b4c9857cfc1594b050
Ivar Flakstad <69173633+ivarflakstad@users.noreply.github.com>
2024-04-13 20:31:01 +0200

    Update nextjs config to use NEXT_PUBLIC_API_URL

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,24 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  async rewrites() {
-    return {
-      beforeFiles: [
-        {
-          source: "/api/:path*",
-          destination: "http://127.0.0.1:9876/:path*",
-        },
-        {
-          source: "/ingest/static/:path*",
-          destination: "https://us-assets.i.posthog.com/static/:path*",
-        },
-        {
-          source: "/ingest/:path*",
-          destination: "https://us.i.posthog.com/:path*",
-        },
-      ],
-    };
-  },
+    reactStrictMode: true,
+    async rewrites() {
+        return {
+            beforeFiles: [
+                {
+                    source: "/api/:path*",
+                    destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
+                },
+                {
+                    source: "/ingest/static/:path*",
+                    destination: "https://us-assets.i.posthog.com/static/:path*",
+                },
+                {
+                    source: "/ingest/:path*",
+                    destination: "https://us.i.posthog.com/:path*",
+                },
+            ],
+        };
+    },
 };
 
 export default nextConfig;
