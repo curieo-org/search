4ac180f3f7434ed36ae7ace618a9e95d12e7eae8
Ivar Flakstad <69173633+ivarflakstad@users.noreply.github.com>
2024-04-15 16:08:20 +0200

    Cleaning up frontend code

--- frontend/src/app/_components/page-content/index.tsx
+++ frontend/src/app/_components/page-content/index.tsx
@@ -1,62 +1,39 @@
 "use client";
 
-import { Suspense, useState } from "react";
-import { SearchResultsGrid } from "../search-results-grid";
-import { SearchForm } from "../search-form";
-import { Loader } from "../loader";
+import {Suspense, useState} from "react";
+import {SearchResultsGrid} from "../search-results-grid";
+import {SearchForm} from "../search-form";
+import {Loader} from "../loader";
 import useSWR from "swr";
 
 async function tokenFetcher(url: string): Promise<boolean> {
-  const c = document.cookie
-    .split("; ")
-    .filter((row) => row.startsWith("id="))
-    .map((c) => c.split("=")[1])[0];
-  console.log(c);
-  if (c) {
+    const input: Record<string, string> = {
+        username: "test1",
+        password: "abcdef",
+    };
+    await fetch(url, {
+        method: "POST",
+        body: new URLSearchParams(input),
+    });
+
     return true;
-  }
-  const input: Record<string, string> = {
-    username: "test1",
-    password: "abcdef",
-  };
-  const payload = new URLSearchParams(input);
-
-  const res = await fetch(url, {
-    method: "POST",
-    body: payload,
-  });
-
-  return true;
 }
 
 export const PageContent = () => {
-  const [searchResults, setSearchResults] = useState<string>("");
-  const {
-    data: token,
-    error,
-    isLoading,
-  } = useSWR("/api/auth/login", tokenFetcher);
+    const {data} = useSWR("/api/auth/login", tokenFetcher);
+    const [searchResults, setSearchResults] = useState<string>("");
+    return (
+        <>
+            <div className="flex flex-col items-center justify-center py-[15vh] sm:py-[20vh]">
+                <div
+                    className="animate-in fade-in slide-in-from-bottom-4 duration-1200 w-full max-w-md space-y-4 ease-in-out">
+                    <SearchForm setSearchResults={setSearchResults}/>
+                </div>
+            </div>
 
-  if (error) {
-    return <h1>Could not ausdsthenticate</h1>;
-  }
-  if (isLoading) {
-    return <Loader />;
-  }
-  if (token === undefined) {
-    return <h1>Could not authenticate</h1>;
-  }
-  return (
-    <>
-      <div className="flex flex-col items-center justify-center py-[15vh] sm:py-[20vh]">
-        <div className="animate-in fade-in slide-in-from-bottom-4 duration-1200 w-full max-w-md space-y-4 ease-in-out">
-          <SearchForm token={token} setSearchResults={setSearchResults} />
-        </div>
-      </div>
-
-      <Suspense>
-        <SearchResultsGrid result={searchResults} />
-      </Suspense>
-    </>
-  );
+            <Suspense>
+                <SearchResultsGrid result={searchResults}/>
+            </Suspense>
+        </>
+    );
 };
--- frontend/src/server/get-search-results-count.ts
+++
@@ -1,3 +0,0 @@
-import "server-only"
-
-export const getSearchResultsCount = async () => 100
