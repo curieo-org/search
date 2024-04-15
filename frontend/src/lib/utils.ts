fadc49d5b638de2711d0e1bfa26af9ed5852dead
Ivar Flakstad <69173633+ivarflakstad@users.noreply.github.com>
2024-04-15 16:15:39 +0200

    Removing more bloat

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,13 +1,6 @@
-import { clsx, type ClassValue } from "clsx"
-import { customAlphabet } from "nanoid"
-import { twMerge } from "tailwind-merge"
+import {clsx, type ClassValue} from "clsx"
+import {twMerge} from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+    return twMerge(clsx(inputs))
 }
-
-export const nanoid = customAlphabet("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz", 10)
-
-export const formatPrompt = (prompt: string) => prompt.replace(/ /g, "-").replace(/-+/g, "-").toLocaleLowerCase()
-
-export const getRandomItem = <T>(items: T[]): T => items[Math.floor(Math.random() * items.length)]
