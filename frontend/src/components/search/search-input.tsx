558eac063a34aab238e7a343fe06dba88c4d8fff
Saifullah Talukder <100260341+saifullah-talukder@users.noreply.github.com>
2024-06-03 07:26:24 +0000

    resolve ui feedbacks by Iana

--- frontend/src/components/search/search-input.tsx
+++ frontend/src/components/search/search-input.tsx
@@ -5,7 +5,7 @@ import { useSearchStore } from '@/stores/search/search-store'
 import classNames from 'classnames'
 import { ChangeEvent, HTMLAttributes, KeyboardEvent, useRef } from 'react'
 import PaperPlaneIcon from '../icons/paper-plane'
-import { Button, IconButton } from '../lib/button'
+import { IconButton } from '../lib/button'
 import { Textarea } from '../lib/form'
 
 type SearchInputProps = HTMLAttributes<HTMLDivElement> & {
@@ -48,15 +48,15 @@ export default function SearchInput(props: SearchInputProps) {
       ref={textAreaRef}
       containerClass="max-w-[880px] grow rounded-2.5xl p-2.5 bg-background-dark/4 dark:bg-background-light/4"
       innerContainerClass={classNames(
-        'rounded-2xl bg-background-light/80 dark:bg-background-dark/80 border border-background-dark/40 dark:border-background-light/40 pr-2 focus-within:border-0 focus-within:outline-none focus-within:ring-2 focus-within:ring-custom-indigo-600/50 focus-within:ring-offset-0',
+        'rounded-2xl bg-background-light/80 dark:bg-background-dark/80 border border-background-dark/40 dark:border-background-light/40 pr-2 focus-within:border-0 focus-within:outline-none focus-within:ring-1 focus-within:ring-primary focus-within:ring-offset-0',
         {
           'py-2': textAreaRef?.current?.style.height === `${maxSearchInputHeight}px`,
         }
       )}
       className={classNames(
-        'h-16 pl-5 py-5 pr-20  rounded-2xl border-none focus-visible:ring-0 bg-background-light/80 dark:bg-background-dark/80 text-typography-light dark:text-typography-dark placeholder:text-typography-light/40 dark:placeholder:text-typography-dark/40 text-sm xl:text-base',
+        'h-16 pl-6 pr-16 rounded-2xl border-none focus-visible:ring-0 bg-background-light/80 dark:bg-background-dark/80 text-typography-light dark:text-typography-dark placeholder:text-typography-light/40 dark:placeholder:text-typography-dark/40 text-sm xl:text-base',
         {
-          'py-5': textAreaRef?.current?.style.height !== `${maxSearchInputHeight}px`,
+          'py-5': textAreaRef?.current?.style.height !== `${maxSearchInputHeight}px` || searchQuery.length === 0,
           'py-3': textAreaRef?.current?.style.height === `${maxSearchInputHeight}px`,
         }
       )}
@@ -67,12 +67,13 @@ export default function SearchInput(props: SearchInputProps) {
       button={
         <IconButton
           className={classNames(
-            'absolute right-8 top-6 h-10 w-12 border border-background-dark/30 dark:border-background-light/30 transition-all duration-700 rounded-lg',
+            'absolute right-6 h-10 w-12 border border-background-dark/30 dark:border-background-light/30 transition-all duration-700 rounded-[10px]',
             {
               'cursor-auto': searchQuery.length === 0,
               'bg-primary/75': searchQuery.length !== 0,
-              'top-6': textAreaRef?.current?.style.height === '64px',
+              'top-6': textAreaRef?.current?.style.height === '64px' || searchQuery.length === 0,
               'bottom-5': textAreaRef?.current?.style.height !== '64px',
+              'right-8': textAreaRef?.current?.style.height === `${maxSearchInputHeight}px`,
             }
           )}
           icon={<PaperPlaneIcon className="text-white/80" size={16} />}
