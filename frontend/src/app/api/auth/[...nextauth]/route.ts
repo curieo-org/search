926512b595450dd19e6066910e57d0c0b011806d
Ivar Flakstad <69173633+ivarflakstad@users.noreply.github.com>
2024-06-07 21:17:54 +0200

    Simplify nextauth router

--- frontend/src/app/api/auth/[...nextauth]/route.ts
+++ frontend/src/app/api/auth/[...nextauth]/route.ts
@@ -1,12 +1,1 @@
-import { GET as G, POST as P } from '@/auth'
-import type { NextRequest } from 'next/server'
-
-export async function GET(req: NextRequest): Promise<Response> {
-  console.debug(req)
-  return G(req)
-}
-
-export async function POST(req: NextRequest): Promise<Response> {
-  console.debug(req)
-  return P(req)
-}
+export { GET, POST } from '@/auth'
