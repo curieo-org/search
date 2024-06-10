32765d989a1da1c570b810b5a0c63c97959bfeb4
Ivar Flakstad <69173633+ivarflakstad@users.noreply.github.com>
2024-06-10 13:37:42 +0200

    Ensure user name / email shows up

--- frontend/src/components/settings/account-details.tsx
+++ frontend/src/components/settings/account-details.tsx
@@ -16,7 +16,7 @@ export default async function AccountDetails(props: AccountDetailsProps) {
       <Span className="mb-4 text-custom-gray-100">Account</Span>
       <div className="mb-2 w-full h-px bg-custom-gray-200/25"></div>
       <div className="px-4 py-3 flex flex-col rounded-2xl bg-background-dark/3 dark:bg-background-light/3">
-        <SettingsInfoItem label="Email" value={session?.user?.email ?? ''} />
+        <SettingsInfoItem label="Email" value={session?.user?.name ?? session?.user?.email ?? ''} />
         <SettingsInfoItem label="Password" value="************" />
       </div>
     </div>
