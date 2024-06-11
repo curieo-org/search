76348ce9c7a297ae3372a7327ac20411ea993062
Rathijit Paul <30369246+rathijitpapon@users.noreply.github.com>
2024-06-11 18:28:37 +0600

    :construction: update prompt for summarizer

--- server/src/llms/bio_llm.rs
+++ server/src/llms/bio_llm.rs
@@ -12,6 +12,7 @@ pub struct BioLLMParams {
     pub model: Option<String>,
     pub max_new_tokens: Option<u16>,
     pub temperature: Option<f32>,
+    pub top_p: Option<f32>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -45,11 +46,20 @@ pub struct BioLLMOutput {
 
 fn prepare_context_string(bio_llm_input: BioLLMInput) -> BioLLMAPIInput {
     BioLLMAPIInput {
-        inputs: format!("Context information is below.\n---------------------\n{}\n---------------------\nUse the tools provided, using the most specific tool available for each action.Your final answer should contain all information necessary to answer the question and subquestions.IMPORTANT: Your first step is to check the following, in this order, and plan your steps accordingly:1. Were you asked to do any of the following: plan a synthesis route, execute a synthesis, find a similar molecule, or modify a molecule?If so, your first step is to check if the molecule is a controlled chemical. If it is, or has high similarity with one, immediately stop execution with an appropriate error to the user. Do not continue.2. Does the question involve any molecules? If so, as a first step, check if any are controlled chemicals. If any are, include a warning in your final answer.3. Were you asked to plan a synthesis route? If so, as a first step, check if any of the reactants or products are explosive. If any are, include a warning in your final answer.4. Were you asked to execute a synthesis route? If so, check if any of the reactants or products are explosive. If any are, ask the user for permission to continue.Do not skip these steps.\nQuery: {}\nAnswer: ", bio_llm_input.retrieved_result, bio_llm_input.query),
+        inputs: format!("In this exercise you will assume the role of a scientific medical assistant. Your task is to answer the provided question as best as you can, based on the provided solution draft.
+        The solution draft follows the format \"Thought, Action, Action Input, Observation\", where the 'Thought' statements describe a reasoning sequence. The rest of the text is information obtained to complement the reasoning sequence, and it is 100% accurate OR you can use a single \"Final Answer\" format.
+        Your task is to write an answer to the question based on the solution draft, and the following guidelines:
+        The text should have an educative and assistant-like tone, be accurate, follow the same reasoning sequence than the solution draft and explain how any conclusion is reached.
+        Question: {}
+
+        Solution draft: {}
+
+        Answer:", bio_llm_input.retrieved_result, bio_llm_input.query),
             parameters: BioLLMParams {
             model: None,
-            max_new_tokens: Some(3000),
-            temperature: Some(0.1),
+            max_new_tokens: Some(1024),
+            temperature: Some(1.0),
+            top_p: Some(0.7),
         },
     }
 }
