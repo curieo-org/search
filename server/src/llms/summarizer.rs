b4a4189bdb36f7ee409e739e1b9767ca93efcfeb
Rathijit Paul <30369246+rathijitpapon@users.noreply.github.com>
2024-06-20 12:42:21 +0600

    :construction: openai summarizer added

--- server/src/llms/summarizer.rs
+++ server/src/llms/summarizer.rs
@@ -1,6 +1,9 @@
+use crate::llms::OpenAISettings;
 use crate::search::api_models;
 use color_eyre::eyre::eyre;
 use futures::StreamExt;
+use regex::Regex;
+use reqwest::header::{HeaderMap, HeaderName, HeaderValue};
 use reqwest::Client;
 use serde::{Deserialize, Serialize};
 use tokio::sync::mpsc::Sender;
@@ -55,7 +58,7 @@ fn prepare_context_string(
         The solution draft follows the format \"Thought, Action, Action Input, Observation\", where the 'Thought' statements describe a reasoning sequence. The rest of the text is information obtained to complement the reasoning sequence, and it is 100% accurate OR you can use a single \"Final Answer\" format.
         Your task is to write an answer to the question based on the solution draft, and the following guidelines:
         The text should have an educative and assistant-like tone, be accurate, follow the same reasoning sequence than the solution draft and explain how any conclusion is reached.
-        Question: {}\n\nSolution draft: {}\n\nAnswer:", summarizer_input.retrieved_result, summarizer_input.query),
+        Question: {}\n\nSolution draft: {}\n\nAnswer:", summarizer_input.query, summarizer_input.retrieved_result),
         parameters: SummarizerParams {
             model: Some(settings.model.clone()),
             max_new_tokens: Some(settings.max_new_tokens.clone()),
@@ -110,6 +113,114 @@ pub async fn generate_text_stream(
             .await
             .map_err(|e| eyre!("Failed to send response: {e}"))?;
         }
+    }
+
+    Ok(())
+}
+
+fn prepare_openai_input(
+    settings: &OpenAISettings,
+    summarizer_input: SummarizerInput,
+) -> serde_json::Value {
+    let system_role = "You are a summarizer AI. In this exercise you will assume the role of a scientific medical assistant. Your task is to answer the provided question as best as you can, based on the provided solution draft.
+    The solution draft follows the format \"Thought, Action, Action Input, Observation\", where the 'Thought' statements describe a reasoning sequence. The rest of the text is information obtained to complement the reasoning sequence, and it is 100% accurate OR you can use a single \"Final Answer\" format.
+    Your task is to write an answer to the question based on the solution draft, and the following guidelines:
+    The text should have an educative and assistant-like tone, be accurate, follow the same reasoning sequence than the solution draft and explain how any conclusion is reached.
+    Question: {}\n\nSolution draft: {}\n\nAnswer: ";
+
+    let user_input = format!(
+        "Question: {}\n\nSolution draft: {}",
+        summarizer_input.query, summarizer_input.retrieved_result
+    );
+
+    serde_json::json!({
+        "model": settings.model,
+        "stream": true,
+        "messages": [
+            {
+                "role": "system",
+                "content": system_role
+            },
+            {
+                "role": "user",
+                "content": user_input
+            }
+        ]
+    })
+}
+
+#[tracing::instrument(level = "debug", ret, err)]
+pub async fn generate_text_with_openai(
+    settings: OpenAISettings,
+    summarizer_input: SummarizerInput,
+    update_processor: api_models::UpdateResultProcessor,
+    stream_regex: Regex,
+    tx: Sender<api_models::SearchByIdResponse>,
+) -> crate::Result<()> {
+    let mut headers = HeaderMap::new();
+    headers.insert(
+        HeaderName::from_bytes(b"Authorization")
+            .map_err(|e| eyre!("Failed to create header: {e}"))?,
+        HeaderValue::from_str(settings.api_key.expose())
+            .map_err(|e| eyre!("Failed to create header: {e}"))?,
+    );
+
+    let client = Client::new();
+    let summarizer_input = prepare_openai_input(&settings, summarizer_input);
+
+    let response = client
+        .post(settings.api_url.as_str())
+        .json(&summarizer_input)
+        .headers(headers)
+        .send()
+        .await
+        .map_err(|e| eyre!("Request to summarizer failed: {e}"))?;
+
+    // stream the response
+    if !response.status().is_success() {
+        return Err(eyre!("Request failed with status: {:?}", response.status()).into());
+    }
+
+    let mut stream = response.bytes_stream();
+    let mut stream_data = String::new();
+
+    while let Some(chunk) = stream.next().await {
+        let chunk = chunk.map_err(|e| eyre!("Failed to read chunk: {e}"))?;
+        stream_data.push_str(&String::from_utf8_lossy(&chunk));
+
+        let parsed_chunk = stream_regex
+            .captures_iter(&stream_data)
+            .map(|c| c[1].to_string())
+            .collect::<Vec<String>>()
+            .join("");
+
+        let last_index = stream_regex
+            .captures_iter(&stream_data)
+            .last()
+            .map(|c| {
+                if let Some(m) = c.get(0) {
+                    return Some(m.end());
+                }
+                None
+            })
+            .unwrap_or(None);
+
+        if let Some(last_index) = last_index {
+            stream_data = stream_data.split_off(last_index);
+        }
+
+        let mut search = update_processor
+            .process(parsed_chunk.clone())
+            .await
+            .map_err(|e| eyre!("Failed to update result: {e}"))?;
+        search.result = parsed_chunk;
+
+        tx.send(api_models::SearchByIdResponse {
+            search,
+            sources: vec![],
+        })
+        .await
+        .map_err(|e| eyre!("Failed to send response: {e}"))?;
     }
 
     Ok(())
--- server/src/rag/post_process.rs
+++ server/src/rag/post_process.rs
@@ -1,8 +1,9 @@
 use crate::llms::summarizer;
-use crate::llms::SummarizerSettings;
 use crate::proto::Embeddings;
 use crate::rag::utils;
 use crate::search::api_models;
+use crate::settings::Settings;
+use regex::Regex;
 use std::cmp::Ordering;
 use tokio::sync::mpsc::Sender;
 
@@ -36,19 +37,21 @@ pub async fn rerank_search_results(
 
 #[tracing::instrument(level = "debug", ret, err)]
 pub async fn summarize_search_results(
-    settings: SummarizerSettings,
+    settings: Settings,
     search_query_request: api_models::SearchQueryRequest,
     search_response: String,
     update_processor: api_models::UpdateResultProcessor,
+    stream_regex: Regex,
     tx: Sender<api_models::SearchByIdResponse>,
 ) -> crate::Result<()> {
-    summarizer::generate_text_stream(
-        settings,
+    summarizer::generate_text_with_openai(
+        settings.openai,
         summarizer::SummarizerInput {
             query: search_query_request.query,
             retrieved_result: search_response,
         },
         update_processor,
+        stream_regex,
         tx,
     )
     .await?;
