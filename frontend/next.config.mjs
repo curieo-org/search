0ed7a5da7f072db929d4245f01472c098f2d6721
Rathijit Paul <30369246+rathijitpapon@users.noreply.github.com>
2024-06-28 11:48:24 +0600

    :construction: fix linting issues and remove unnecessary packages

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,4 @@
-import {withSentryConfig} from '@sentry/nextjs';
+import { withSentryConfig } from '@sentry/nextjs'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
@@ -30,8 +30,8 @@ export default withSentryConfig(nextConfig, {
   // For all available options, see:
   // https://github.com/getsentry/sentry-webpack-plugin#options
 
-  org: "curieo",
-  project: "search",
+  org: 'curieo',
+  project: 'search',
 
   // Only print logs for uploading source maps in CI
   silent: !process.env.CI,
@@ -46,7 +46,7 @@ export default withSentryConfig(nextConfig, {
   // This can increase your server load as well as your hosting bill.
   // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
   // side errors will fail.
-  tunnelRoute: "/monitoring",
+  tunnelRoute: '/monitoring',
 
   // Hides source maps from generated client bundles
   hideSourceMaps: true,
@@ -59,4 +59,4 @@ export default withSentryConfig(nextConfig, {
   // https://docs.sentry.io/product/crons/
   // https://vercel.com/docs/cron-jobs
   automaticVercelMonitors: true,
-});
+})
