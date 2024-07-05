c65d25f5ce49482396d10da25056a7f28a7ed8c5
Saifullah Talukder <100260341+saifullah-talukder@users.noreply.github.com>
2024-07-05 15:32:05 +0000

    fix markdown style

--- frontend/src/components/search/search-response.tsx
+++ frontend/src/components/search/search-response.tsx
@@ -1,24 +1,34 @@
-import { HTMLAttributes } from 'react'
+import { HTMLAttributes, OlHTMLAttributes } from 'react'
 import Markdown from 'react-markdown'
-import remarkGfm from 'remark-gfm'
 import { twMerge } from 'tailwind-merge'
 
 type SearchResponseProps = HTMLAttributes<HTMLDivElement> & {
   response: string
 }
 
+const components = {
+  ol(props: OlHTMLAttributes<HTMLOListElement>) {
+    return (
+      <ol className="list-decimal pl-4" start={props.start}>
+        {props.children}
+      </ol>
+    )
+  },
+  ul(props: HTMLAttributes<HTMLUListElement>) {
+    return <ul className="list-disc pl-8">{props.children}</ul>
+  },
+  h3(props: HTMLAttributes<HTMLHeadingElement>) {
+    return <h3 className="font-bold text-lg">{props.children}</h3>
+  },
+}
+
 export default function SearchResponse(props: SearchResponseProps) {
   return (
     <div className={twMerge('flex flex-col gap-y-2', props.className)}>
       {props.response.split('\\n').map((paragraph, index) => (
-        <div
-          key={`response-paragraph-${index}`}
-          //style={{ animation: `fade-in ${Math.min(1 + index * 0.5, 5)}s` }}
-        >
-          <Markdown className="text-sm text-white/80" remarkPlugins={[remarkGfm]}>
-            {paragraph}
-          </Markdown>
-        </div>
+        <Markdown key={`response-paragraph-${index}`} className="text-base text-white/80" components={components}>
+          {paragraph.replaceAll('    -', '   -')}
+        </Markdown>
       ))}
     </div>
   )
