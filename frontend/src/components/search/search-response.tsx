9358ce61e3a76ae822d06dcd98e7c0f10df4d354
Saifullah Talukder <100260341+saifullah-talukder@users.noreply.github.com>
2024-07-05 07:34:02 +0000

    integrate markdown feature

--- frontend/src/components/search/search-response.tsx
+++ frontend/src/components/search/search-response.tsx
@@ -1,6 +1,7 @@
 import { HTMLAttributes } from 'react'
+import Markdown from 'react-markdown'
+import remarkGfm from 'remark-gfm'
 import { twMerge } from 'tailwind-merge'
-import { P } from '../lib/typography'
 
 type SearchResponseProps = HTMLAttributes<HTMLDivElement> & {
   response: string
@@ -10,13 +11,11 @@ export default function SearchResponse(props: SearchResponseProps) {
   return (
     <div className={twMerge('flex flex-col gap-y-2', props.className)}>
       {props.response.split('\\n').map((paragraph, index) => (
-        <P
-          className="pr-4 text-sm text-white/80"
-          style={{ animation: `fade-in ${Math.min(1 + index * 0.5, 5)}s` }}
-          key={`response-paragraph-${index}`}
-        >
-          {paragraph}
-        </P>
+        <div key={`response-paragraph-${index}`} style={{ animation: `fade-in ${Math.min(1 + index * 0.5, 5)}s` }}>
+          <Markdown className="text-sm text-white/80" remarkPlugins={[remarkGfm]}>
+            {paragraph}
+          </Markdown>
+        </div>
       ))}
     </div>
   )
